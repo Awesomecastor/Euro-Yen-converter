99fdbde1da56bee981f59c1b2f562221290d53b2
ElieGehin <etgehin@gmail.com>
2021-01-18 12:40:28 +0100

    Converter made functional

--- Euro-Yen converter.js
+++ Euro-Yen converter.js
@@ -1,10 +1,24 @@
-function converter (amount, money) {
-if (money === "€") {
-    return amount + " € = " + amount*125.47 + " ¥";
-}
-else if (money === "¥") {
-    return amount + " ¥ = " + amount*0.0080 + " €";
-}
-}
-var result = converter(400, "€");
-console.log(result);
+function converter(amount, money) {
+    if (money === "€") {
+      return amount + " € = " + amount * 125.47 + " ¥";
+    } else if (money === "¥") {
+      return amount + " ¥ = " + amount * 0.008 + " €";
+    }
+  }
+  function displayResult() {
+    var amount = document.getElementById("amount").value;
+    var money = document.getElementById("money").value;
+    var displayResult = document.getElementById("displayResult");
+    var result = converter(amount, money);
+    var conversion = document.getElementById("conversion");
+    if (conversion === null){
+    var paragraph = document.createElement("p");
+    paragraph.setAttribute("id", "conversion")
+    displayResult.appendChild(paragraph);
+    paragraph.innerHTML = result;
+    }
+    else {
+        conversion.innerHTML = result;
+    }
+  }
+
